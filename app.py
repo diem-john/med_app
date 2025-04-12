bb4c741a53d431f0aeeaa65a6b4e9728134b2731
Daryll John <104271234+diem-john@users.noreply.github.com>
2025-04-12 18:06:47 +0800

    Update app.py
    Added Image
    And other Computations

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from methods import *
 
 st.set_page_config(
@@ -23,17 +24,21 @@ linear-gradient(45deg, #FFB6C9 25%, #FFB6C1 25%) 0px 0/ 20px 20px;
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-st.title('Meds Tracker & Inventory')
+st.title('meds tracker & inventory')
 st.subheader(f'Date: {datetime.now().strftime("%m-%d-%Y")}')
 
-st.sidebar.title('Task Management')
-task_ = st.sidebar.selectbox('Select Task:',
+st.sidebar.title('task management')
+task_ = st.sidebar.selectbox('select task:',
                              ('',
                               'Add New Medicine',
                               'Update Medicine',
                               'Delete Item'))
 
-st.sidebar.header('Inventory Management')
+st.sidebar.header('inventory management')
+if task_ == '':
+    image = Image.open('img/logo.JPG')
+    st.sidebar.image(image, caption='Liasaurus')
+
 if task_ == 'Add New Medicine':
     st.sidebar.subheader('Add New Medicine to Inventory')
     with st.sidebar.form('add_medicine_form'):
@@ -47,23 +52,12 @@ if task_ == 'Add New Medicine':
         price = st.number_input("Price (₱): ")
         notes = st.text_input("Enter any notes (optional): ").strip()
         medicine = (generic_name, brand_name, schedule_8am, schedule_1pm,
-                    schedule_8pm, intended_duration_days, doses_left, price, notes)
+                    schedule_8pm, intended_duration_days, doses_left, price, notes, datetime.now().strftime("%m%d%Y"))
         submit_button = st.form_submit_button("Add this entry?")
         if submit_button:
             if not generic_name:
                 st.error("Generic Name is required.")
             else:
-                medicine = (
-                    generic_name,
-                    brand_name,
-                    schedule_8am,
-                    schedule_1pm,
-                    schedule_8pm,
-                    intended_duration_days,
-                    doses_left,
-                    price,
-                    notes,
-                )
                 medicine_id = add_medicine(conn, medicine)
                 if medicine_id:
                     st.success(f"Medicine '{generic_name}' added!")
@@ -99,25 +93,17 @@ elif task_ == 'Update Medicine':
 
         update_button = st.form_submit_button("Update")
         if update_button:
-            update_data = {}
-            if generic_name != selected_medicine[1]:
-                update_data["generic_name"] = generic_name
-            if brand_name != selected_medicine[2]:
-                update_data["brand_name"] = brand_name
-            if converter(schedule_8am) != selected_medicine[3]:
-                update_data["schedule_8am"] = converter(schedule_8am)
-            if converter(schedule_1pm) != selected_medicine[4]:
-                update_data["schedule_1pm"] = converter(schedule_1pm)
-            if converter(schedule_8pm) != selected_medicine[5]:
-                update_data["schedule_8pm"] = converter(schedule_8pm)
-            if intended_duration_days != selected_medicine[6]:
-                update_data["intended_duration_days"] = intended_duration_days
-            if doses_left != selected_medicine[7]:
-                update_data["doses_left"] = doses_left
-            if price != selected_medicine[8]:
-                update_data["price"] = price
-            if notes != selected_medicine[9]:
-                update_data["notes"] = notes
+            update_data = {
+                "generic_name": generic_name,
+                "brand_name": brand_name,
+                "schedule_8am": converter(schedule_8am),
+                "schedule_1pm": converter(schedule_1pm),
+                "schedule_8pm": converter(schedule_8pm),
+                "intended_duration_days": intended_duration_days,
+                "doses_left": doses_left,
+                "price": price,
+                "notes": notes,
+            }
 
             if update_data:  # Only update if there are changes
                 if update_medicine(conn, medicine_id_to_update, update_data):
