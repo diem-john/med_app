74d1ab33d449ce9223f2774a32856eab3bdcdb12
Daryll John <104271234+diem-john@users.noreply.github.com>
2025-04-12 14:56:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,6 @@ if task_ == 'Add New Medicine':
                 medicine_id = add_medicine(conn, medicine)
                 if medicine_id:
                     st.success(f"Medicine '{generic_name}' added!")
-                    display_inventory_streamlit(conn)
                 else:
                     st.error(f"Failed to add medicine '{generic_name}'.")
 elif task_ == 'Update Medicine':
@@ -64,9 +63,9 @@ elif task_ == 'Update Medicine':
     with st.sidebar.form("update_medicine_form"):
         generic_name = st.text_input("Generic Name", value=selected_medicine[1], key="update_generic_name")
         brand_name = st.text_input("Brand Name (optional)", value=selected_medicine[2] or "", key="update_brand_name")
-        schedule_8am = st.checkbox("8 AM", value=bool(selected_medicine[3]), key="update_schedule_8am")
-        schedule_1pm = st.checkbox("1 PM", value=bool(selected_medicine[4]), key="update_schedule_1pm")
-        schedule_8pm = st.checkbox("8 PM", value=bool(selected_medicine[5]), key="update_schedule_8pm")
+        schedule_8am = st.checkbox("8 AM", value=selected_medicine[3], key="update_schedule_8am")
+        schedule_1pm = st.checkbox("1 PM", value=selected_medicine[4], key="update_schedule_1pm")
+        schedule_8pm = st.checkbox("8 PM", value=selected_medicine[5], key="update_schedule_8pm")
         intended_duration_days = st.number_input("Intended Duration (days)", min_value=1, value=selected_medicine[6],
                                                  key="update_intended_duration")
         doses_left = st.number_input("Current Doses Left", min_value=0, value=selected_medicine[7],
@@ -74,8 +73,8 @@ elif task_ == 'Update Medicine':
         price = st.number_input("Price", min_value=0.0, format="%.2f", value=selected_medicine[8] or 0,
                                 key="update_price")
         notes = st.text_area("Notes (optional)", value=selected_medicine[9] or "", key="update_notes")
-        update_button = st.form_submit_button("Update")
 
+        update_button = st.form_submit_button("Update")
         if update_button:
             update_data = {}
             if generic_name != selected_medicine[1]:
